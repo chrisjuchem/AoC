289c693d64d4f84a1fb2d83ef571a00618cd8fe7
Chris Juchem <chrisjuchem15@gmail.com>
2024-12-02 19:41:36 -0500

    add template

---
+++ src/template/mod.rs
@@ -0,0 +1,56 @@
+use crate::aoc_day;
+use crate::{AocDay, AocYear};
+
+pub mod day01;
+pub mod day02;
+pub mod day03;
+pub mod day04;
+pub mod day05;
+pub mod day06;
+pub mod day07;
+pub mod day08;
+pub mod day09;
+pub mod day10;
+pub mod day11;
+pub mod day12;
+pub mod day13;
+pub mod day14;
+pub mod day15;
+pub mod day16;
+pub mod day17;
+pub mod day18;
+pub mod day19;
+pub mod day20;
+pub mod day21;
+pub mod day22;
+pub mod day23;
+pub mod day24;
+pub mod day25;
+
+pub const DAYS: AocYear = [
+    aoc_day!(day01),
+    aoc_day!(day02),
+    aoc_day!(day03),
+    aoc_day!(day04),
+    aoc_day!(day05),
+    aoc_day!(day06),
+    aoc_day!(day07),
+    aoc_day!(day08),
+    aoc_day!(day09),
+    aoc_day!(day10),
+    aoc_day!(day11),
+    aoc_day!(day12),
+    aoc_day!(day13),
+    aoc_day!(day14),
+    aoc_day!(day15),
+    aoc_day!(day16),
+    aoc_day!(day17),
+    aoc_day!(day18),
+    aoc_day!(day19),
+    aoc_day!(day20),
+    aoc_day!(day21),
+    aoc_day!(day22),
+    aoc_day!(day23),
+    aoc_day!(day24),
+    aoc_day!(day25),
+];
