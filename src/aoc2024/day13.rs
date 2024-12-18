35a4489e9bfe575bf58a304dcc58049173bcf804
Chris Juchem <chrisjuchem15@gmail.com>
2024-12-18 12:13:13 -0500

    day 13 pt 2

--- src/aoc2024/day13.rs
+++ src/aoc2024/day13.rs
@@ -2,7 +2,7 @@ use crate::grid::{DeltaLoc, Loc};
 use crate::util::{aoc_test, CollectVec};
 use regex::Regex;
 
-const START: Loc = Loc { r: 0, c: 0 };
+const N: f64 = 10000000000000.;
 
 struct Game {
     a: DeltaLoc,
@@ -13,9 +13,10 @@ impl Game {
     fn solve(&self) -> Option<(i64, i64)> {
         let mut na = 0;
         let mut nb = 100;
+        let start = Loc { r: 0, c: 0 };
 
         while na < 100 {
-            let claw = START + self.a * na + self.b * nb;
+            let claw = start + self.a * na + self.b * nb;
 
             if claw.r == self.target.r && claw.c == self.target.c {
                 return Some((na, nb));
@@ -29,9 +30,47 @@ impl Game {
         }
         None
     }
+
+    fn solve_large(&self) -> Option<(i64, i64)> {
+        let slope_a = self.a.dc as f64 / self.a.dr as f64;
+        let slope_b = self.b.dc as f64 / self.b.dr as f64;
+
+        // y= mx+b
+        // A: 0,0                              ->  y = slope_a * x + 0
+        // B: N,N  ->  N = slope_b * N + icpt  ->  y = slope_b * x + (N - slope_b * N)
+        //                             0 = (slope_a - slope_b) * x - (N - slope_b * N)
+        let x = (N - slope_b * N) / (slope_a - slope_b);
+        // let y = slope_a * x;
+
+        let nai = x as i64 / self.a.dr;
+        let nbi = (N - x) as i64 / self.b.dr;
+
+        let start = Loc {
+            r: (self.a.dr * nai + self.b.dr * nbi) - N as i64,
+            c: (self.a.dc * nai + self.b.dc * nbi) - N as i64,
+        };
+
+        let mut na = -1000;
+        let mut nb = 1000;
+
+        while na < 1000 && nb > -1000 {
+            let claw = start + self.a * na + self.b * nb;
+
+            if claw.r == self.target.r && claw.c == self.target.c {
+                return Some((na + nai, nb + nbi));
+            }
+
+            if claw.r >= self.target.r || claw.c >= self.target.c {
+                nb -= 1;
+            } else {
+                na += 1;
+            }
+        }
+        None
+    }
 }
 
-pub fn part1(input: String) -> u64 {
+fn tokens(input: String, solve: impl FnMut(&Game) -> Option<(i64, i64)>) -> u64 {
     let re = Regex::new(
         r"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)",
     )
@@ -57,14 +96,18 @@ pub fn part1(input: String) -> u64 {
         .collect_vec();
 
     games
-        .into_iter()
-        .filter_map(|g| g.solve())
+        .iter()
+        .filter_map(solve)
         .map(|(a, b)| (a * 3 + b) as u64)
         .sum()
 }
 
-pub fn part2(_input: String) -> u64 {
-    0
+pub fn part1(input: String) -> u64 {
+    tokens(input, Game::solve)
+}
+
+pub fn part2(input: String) -> u64 {
+    tokens(input, Game::solve_large)
 }
 
 aoc_test!(
@@ -84,5 +127,5 @@ Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279",
     480,
-    0,
+    875318608908,
 );
