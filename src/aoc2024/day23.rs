16d36379921563a46d98999c4dcc3f7e46864881
Chris Juchem <chrisjuchem15@gmail.com>
2024-12-28 22:44:17 -0500

    day23pt2

--- src/aoc2024/day23.rs
+++ src/aoc2024/day23.rs
@@ -35,8 +35,58 @@ pub fn part1(input: String) -> usize {
         .count()
 }
 
-pub fn part2(_input: String) -> u64 {
-    0
+fn can_join(conns: &HashMap<&str, BTreeSet<&str>>, party: &[&str], candidate: &str) -> bool {
+    for c in party {
+        let Some(c_conns) = conns.get(c) else {
+            return false;
+        };
+
+        if !c_conns.contains(candidate) {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn recurse<'a>(party: Vec<&'a str>, conns: &HashMap<&str, BTreeSet<&'a str>>) -> Vec<&'a str> {
+    let Some(candidates) = conns.get(party[party.len() - 1]) else {
+        return party;
+    };
+
+    let mut best = party.clone();
+    for c in candidates {
+        if can_join(conns, &party, c) {
+            let mut new_party = party.clone();
+            new_party.push(c);
+            let r = recurse(new_party, conns);
+
+            if r.len() > best.len() {
+                best = r;
+            }
+        }
+    }
+
+    best
+}
+
+pub fn part2(input: String) -> String {
+    let mut connections: HashMap<&str, BTreeSet<_>> = HashMap::new();
+
+    for line in input.lines() {
+        let (mut a, mut b) = line.split_into("-");
+        if a > b {
+            (a, b) = (b, a);
+        }
+
+        connections.entry(a).or_default().insert(b);
+    }
+
+    connections
+        .keys()
+        .map(|comp| recurse(vec![*comp], &connections))
+        .max_by_key(|party| party.len())
+        .unwrap()
+        .join(",")
 }
 
 aoc_test!(
@@ -74,5 +124,5 @@ tb-vc
 td-yn
 ",
     7,
-    0,
+    "co,de,ka,ta",
 );
