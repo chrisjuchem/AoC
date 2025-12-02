8750b5b332ce7f407449265a4f49d3e5f1d2f949
Chris Juchem <chrisjuchem15@gmail.com>
2025-12-02 07:08:05 -0500

    2025 day 1

--- src/aoc2025/day01.rs
+++ src/aoc2025/day01.rs
@@ -1,11 +1,68 @@
+use num::Integer;
+
 use crate::util::aoc_test;
 
-pub fn part1(_input: String) -> u64 {
-    0
+pub fn part1(input: String) -> u64 {
+    let mut dial = 50i32;
+    let mut zeros = 0;
+    for line in input.lines() {
+        let mut mul = 1;
+        if line.chars().next().unwrap() == 'L' {
+            mul = -1;
+        }
+
+        let n = line[1..].parse::<i32>().unwrap();
+        dial += n * mul;
+
+        if dial.is_multiple_of(&100) {
+            zeros += 1;
+        }
+    }
+
+    zeros
 }
 
-pub fn part2(_input: String) -> u64 {
-    0
+pub fn part2(input: String) -> u64 {
+    let mut dial = 50i32;
+    let mut zeros = 0;
+    for line in input.lines() {
+        let mut mul = 1;
+        if line.chars().next().unwrap() == 'L' {
+            mul = -1;
+        }
+        let n = mul * line[1..].parse::<i32>().unwrap();
+
+        let start = dial;
+        dial += n;
+
+        if start != 0 && start * dial <= 0 {
+            zeros += 1;
+        }
+
+        while dial < -99 {
+            zeros += 1;
+            dial += 100;
+        }
+        while dial > 99 {
+            zeros += 1;
+            dial -= 100;
+        }
+    }
+
+    zeros
 }
 
-aoc_test!("", 0, 0,);
+aoc_test!(
+    "L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82",
+    3,
+    6,
+);
