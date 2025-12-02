dd367fdceec22fe7750f6984118780352847d7b1
Chris Juchem <chrisjuchem15@gmail.com>
2025-12-02 07:52:31 -0500

    day2

--- src/aoc2025/day02.rs
+++ src/aoc2025/day02.rs
@@ -1,11 +1,66 @@
-use crate::util::aoc_test;
+use num::Integer;
 
-pub fn part1(_input: String) -> u64 {
-    0
+use crate::util::{SplitInto, aoc_test};
+
+pub fn part1(input: String) -> u64 {
+    let mut sum = 0;
+
+    for range_str in input.split(',') {
+        let (min, max) = range_str.trim().split_into("-");
+        let min: u64 = min.parse().unwrap();
+        let max = max.parse().unwrap();
+
+        for n in min..=max {
+            let s = format!("{n}");
+            if s.len().is_odd() {
+                continue;
+            };
+            let len = s.len() / 2;
+            if s[..len] == s[len..] {
+                sum += n;
+            }
+        }
+    }
+
+    sum
 }
 
-pub fn part2(_input: String) -> u64 {
-    0
+pub fn part2(input: String) -> u64 {
+    let mut sum = 0;
+
+    for range_str in input.split(',') {
+        let (min, max) = range_str.trim().split_into("-");
+        let min: u64 = min.parse().unwrap();
+        let max = max.parse().unwrap();
+
+        'n: for n in min..=max {
+            let s = format!("{n}");
+            let len = s.len();
+
+            'pat: for pat_len in 1..=len / 2 {
+                if !len.is_multiple_of(pat_len) {
+                    continue;
+                }
+
+                for i in 1..len / pat_len {
+                    if s[0..pat_len] != s[pat_len * i..pat_len * (i + 1)] {
+                        continue 'pat;
+                    }
+                }
+
+                sum += n;
+                continue 'n;
+            }
+        }
+    }
+
+    sum
 }
 
-aoc_test!("", 0, 0,);
+aoc_test!(
+    "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124",
+    1227775554,
+    4174379265,
+);
