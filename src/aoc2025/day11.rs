0be70c93fe5aa62b194ceb34f20f418f0834ba3e
Chris Juchem <chrisjuchem15@gmail.com>
2025-12-11 22:27:44 -0500

    day 11

--- src/aoc2025/day11.rs
+++ src/aoc2025/day11.rs
@@ -1,11 +1,99 @@
-use crate::util::aoc_test;
+use std::collections::HashMap;
 
-pub fn part1(_input: String) -> u64 {
-    0
+use crate::util::{CollectVec, SplitInto, aoc_test};
+
+fn lookup(node: &str, map: &HashMap<&str, Vec<&str>>) -> u64 {
+    let mut paths = 0;
+    if node == "out" {
+        return 1;
+    }
+
+    for next in map[node].iter() {
+        paths += lookup(next, map);
+    }
+
+    paths
 }
 
-pub fn part2(_input: String) -> u64 {
-    0
+fn lookup2(
+    node: &str,
+    map: &HashMap<&str, Vec<&str>>,
+    mut found_dac: bool,
+    mut found_fft: bool,
+    cache: &mut HashMap<(String, bool, bool), u64>,
+) -> u64 {
+    let mut paths = 0;
+    if node == "out" {
+        return (found_dac && found_fft).then_some(1).unwrap_or(0);
+    }
+    if node == "fft" {
+        found_fft = true;
+    }
+    if node == "dac" {
+        found_dac = true;
+    }
+
+    for next in map[node].iter() {
+        let cache_key = (next.to_string(), found_dac, found_fft);
+        let n = match cache.get(&cache_key) {
+            None => {
+                let n = lookup2(next, map, found_dac, found_fft, cache);
+                cache.insert(cache_key, n);
+                n
+            }
+            Some(n) => *n,
+        };
+        paths += n;
+    }
+
+    paths
 }
 
-aoc_test!("", 0, 0,);
+pub fn part1(input: String) -> u64 {
+    let mut map = HashMap::new();
+    for line in input.lines() {
+        let (node, conns) = line.split_into(": ");
+        map.insert(node, conns.split(" ").collect_vec());
+    }
+
+    lookup("you", &map)
+}
+
+pub fn part2(input: String) -> u64 {
+    let mut map = HashMap::new();
+    for line in input.lines() {
+        let (node, conns) = line.split_into(": ");
+        map.insert(node, conns.split(" ").collect_vec());
+    }
+
+    lookup2("svr", &map, false, false, &mut HashMap::new())
+}
+
+aoc_test!(
+    "aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out
+",
+    5,
+    "svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out",
+    2,
+);
